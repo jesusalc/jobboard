bba6f6f1b06e4cb693bde9af820791d2c6da8ccb
Jesus Alcaras <jesusalc@gmail.com>
2013-04-14 06:09:27 -0700

    Rails New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobboard::Application.config.secret_token = '77df1171abcde0b19d1d7f0e9d3d454fddb10be5986e14917c01068dbc70b9f7d30553eaefdd28807db752c371df1edb370f4956535399e10e4ae2cedac718d6'
